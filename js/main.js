7ece8825a4584413511f5e27844f359307046f44
rachit2400 <84562866+rachit2400@users.noreply.github.com>
2021-05-31 01:39:50 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ var x = document.getElementById("myAudio");
 function playAudio() {
   x.play();
 }
+
+function page(){
+  alert('Congratulations Yavnika! You have downloaded a super duper app! Hope you love it baee!!');
+  window.open("https://www.w3schools.com");
+}
